3c9d4fc1ecd2983d341c1c4b689c8327ca7e9f12
lior schwartz <lior@soltrex.io>
2021-10-16 17:57:44 +0300

    added class imprt example. nothing critical

---
+++ src/misc_scripts/class_import.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from pythonclass import myclass
+
+if __name__ == '__main__':
+    my_class=myclass("lior")
---
+++ src/misc_scripts/pythonclass.py
@@ -0,0 +1,4 @@
+
+class myclass():
+    def __init__(self,name):
+        print("my name is: ", name)
