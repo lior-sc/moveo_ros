5293759262d78d45c269fb929e668760721b89a4
lior schwartz <lior@soltrex.io>
2021-10-16 18:02:30 +0300

    same

--- src/misc_scripts/class_import.py
+++ src/misc_scripts/class_import.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from pythonclass import myclass
+# good tutorial: https://docs.python.org/3/tutorial/modules.html#packages
+from classes.pythonclass import myclass
 
 if __name__ == '__main__':
     my_class=myclass("lior")
