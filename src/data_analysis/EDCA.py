fc135a16a477a8a667fdce21397710f1bf9e5936
lior schwartz <lior@soltrex.io>
2022-02-06 23:30:30 +0200

    working on euclidean clustering algorithm

--- src/data_analysis/EDCA.py
+++ src/data_analysis/EDCA.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+
 import rospy
 import numpy as np
-from copy import deepcopy                          #https://answers.ros.org/question/37682/python-deep-copy-of-ros-message/
+from copy import deepcopy  #https://answers.ros.org/question/37682/python-deep-copy-of-ros-message/
 
-import pandas as pd
-from matplotlib import pyplot as plt
+
 from sensor_msgs.msg import LaserScan
 
 filter_pub = rospy.Publisher('/filtered_scan', LaserScan, queue_size=1000)
@@ -13,9 +13,8 @@ cluster1_pub = rospy.Publisher('/C1_scan', LaserScan, queue_size=1000)
 cluster2_pub = rospy.Publisher('/C2_scan', LaserScan, queue_size=1000)
 cluster3_pub = rospy.Publisher('/C3_scan', LaserScan, queue_size=1000)
 
-def filter_scan(ranges, angles):
-    r = np.asarray(deepcopy(ranges))
-    theta = deepcopy(angles)
+def filter_scan(ranges, theta):
+    r = np.asarray(ranges)
 
     max_range = 1.5 #[m]
     angle_mask = np.ones(720)
@@ -37,15 +36,27 @@ def filter_scan(ranges, angles):
 def ECA_callback(scan):
     angles_array = theta=np.linspace(np.pi/2, (5/2)*np.pi, len(scan.ranges), endpoint=False)
     
-    ### filter scan
+    ### filter scan to remove unwated angles and ranges
     r, theta, filtered_scan_indices = filter_scan(scan.ranges, angles_array)
 
     ### calculate euclidean distance between adjacent points
-    # Dist = 
+    d = []
+    for i in range(1,np.size(r)):
+        euclidean_distance = np.sqrt(r[i-1]**2+r[i]**2-2*r[i-1]*r[i]*np.cos(theta[i-1]-theta[i]))   # https://socratic.org/questions/what-is-the-formula-for-the-distance-between-two-polar-coordinates 
+        d.append(euclidean_distance)
+        ## need to find a faster way than a python loop. can be done via numpy
     
-    ## data preperation: convert from polar to catesian coordinate system
-    X = np.transpose(np.array([r*np.sin(theta),r*np.cos(theta)]))
-                                       
+    ### cluster allocation     
+    max_cluster_seperation = 0.005  #[m]
+    cluster_index = 0
+    cluster_allocation_vector = [cluster_index]
+
+    for i in range(1,np.size(r)):
+        if np.absolute(d[i-1])>max_cluster_seperation:
+            cluster_index += 1
+        cluster_allocation_vector.append(cluster_index)
+
+    ## we still need to create scan topics for the clusters. this will be done tomorrow                                     
 
     return
 
